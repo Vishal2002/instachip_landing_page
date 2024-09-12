47d893d5ebb4240eb93173105d40c1785dbc45cf
Vishal <vishalsharma05052002@gmail.com>
2024-09-12 21:44:01 +0530

    redefined

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,33 @@
 import React, { useEffect } from 'react';
 import { Terminal } from 'lucide-react';
 import Logo from '@/component/ui/Logo';
-import Navbar from '@/component/ui/Navbar';
 
+const Navbar = () => {
+  return (
+    <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+       <Logo/>
+        <div className="flex gap-2 md:order-2">
+          <button type="button" className="text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:outline-none focus:ring-purple-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-800">Download</button>
+          <button type="button" className="text-purple-700 bg-white hover:bg-gray-100 border border-purple-700 focus:ring-4 focus:outline-none focus:ring-purple-300 font-medium rounded-lg text-sm px-4 py-2 text-center ml-3 md:ml-0 dark:text-purple-400 dark:border-purple-400 dark:hover:bg-gray-700">Contact</button>
+        </div>
+        <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
+          <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+            <li>
+              <a href="#home" className="block py-2 pl-3 pr-4 text-white bg-purple-700 rounded md:bg-transparent md:text-purple-700 md:p-0 md:dark:text-purple-500" aria-current="page">Home</a>
+            </li>
+            <li>
+              <a href="#about" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-purple-700 md:p-0 md:dark:hover:text-purple-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">About</a>
+            </li>
+            <li>
+              <a href="#services" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-purple-700 md:p-0 md:dark:hover:text-purple-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Services</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  )
+}
 
 
 const Home = () => {
